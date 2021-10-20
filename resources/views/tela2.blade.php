cdea34bda0a9b7b99c57a1a817f4612c8096099f
Lucas Santiago <60247372+Lucsallk@users.noreply.github.com>
2021-10-19 22:23:34 -0300

    Create tela2.blade.php

---
+++ resources/views/tela2.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+
+    <title>Lista de Produtos</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Tabela de Detalhes</h1>
+        <table class="table table-hover ">
+            <thead>
+              <th>ID</th>
+                 <th>Nome</th>
+                 <th>Categoria</th>
+                 <th>Quantidade de Itens</th>
+                 <th>AVG Vendas</th>
+            </thead>
+            <tbody>
+              <tr>
+                <td>0001</td>
+                <td>Macarrão</td>
+                <td>Alimento</td>
+                <td>x</td>
+                <td>y</td>
+              </tr>
+              <tr>
+                <td>0002</td>
+                <td>Tomate</td>
+                <td>Alimentos</td>
+                <td>x</td>
+                <td>y</td>
+              </tr>
+              <tr>
+                <td>0101</td>
+                <td>Pasta de Dentes</td>
+                <td>Higiene Pessoal</td>
+                <td>x</td>
+                <td>y</td>
+              </tr>
+              <tr>
+                <td>0102</td>
+                <td>Martelo</td>
+                <td>Ferramentas</td>
+                <td>x</td>
+                <td>y</td>
+              </tr>
+            </tbody>
+          </table>
+          <a href="tela1.blade.php">Tabela de Listagem</a>
+    </div>
+</body>
+</html>
