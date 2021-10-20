434636a3708b978989e12117f79f7a4bf2734520
Lucas Santiago <60247372+Lucsallk@users.noreply.github.com>
2021-10-19 22:22:26 -0300

    Create tela1.blade.php

---
+++ resources/views/tela1.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+
+    <title>Lista de Produtos</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Tabela de Listagem</h1>
+        <table class="table table-hover ">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Categoria</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>0001</td>
+                <td>Macarrão</td>
+                <td>Alimento</td>
+              </tr>
+              <tr>
+                <td>0002</td>
+                <td>Tomate</td>
+                <td>Alimentos</td>
+              </tr>
+              <tr>
+                <td>0101</td>
+                <td>Pasta de Dentes</td>
+                <td>Higiene Pessoal</td>
+              </tr>
+              <tr>
+                <td>0102</td>
+                <td>Martelo</td>
+                <td>Ferramentas</td>
+              </tr>
+            </tbody>
+          </table>
+          <a href="tela2.html">Tabela de Detalhes</a>
+    </div>
+</body>
+</html>
